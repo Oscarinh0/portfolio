ac67d72eedb5b570f9e7b6c4b0b5bc407aad46cc
oscar bonte <inbox.ob@gmail.com>
2018-09-27 14:59:33 -0300

    db done

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_12_191342) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
