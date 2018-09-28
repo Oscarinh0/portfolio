56dfd7b79f313db0e6b6629f2408e7ece4242c7c
oscar bonte <inbox.ob@gmail.com>
2018-09-28 16:07:48 -0300

    buttons fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get 'projects/show'
-  get 'home', to: 'pages#home', as: :home
-  get 'contact', to: 'pages#contact', as: :contact
-  get 'inspirations', to: 'pages#inspirations', as: :inspirations
-  get "projects", to: "projects#index", as: :projects
+	root to: 'pages#home'
+	get 'projects/show'
+	get 'home', to: 'pages#home', as: :home
+	get 'contact', to: 'pages#contact', as: :contact
+	get 'inspirations', to: 'pages#inspirations', as: :inspirations
+	get "projects", to: "projects#index", as: :projects
 end
 
 
