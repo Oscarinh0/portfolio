412ff4d326003535edf1843351a40dce9d737a37
oscar bonte <inbox.ob@gmail.com>
2018-10-03 21:00:55 -0300

    cloudinary done

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'oscarinh0'
+  config.api_key = '498942124386761'
+  config.api_secret = 'aGn590CHrEYBKpNb9sTL02tLZM4'
+  config.secure = true
+  config.cdn_subdomain = true
+end
